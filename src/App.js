a617436032a17df50ad928d043f1d2674a52938d
Igor Ershov <privetie@yandex.ru>
2022-12-16 23:28:33 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="bigCircle" />
+      <div className="line" />
+      <div className="smallCircle" />
+      <div className="oval" />
+      <div className="triangle" />
+    </div>
+  );
+}
+
+export default App;
